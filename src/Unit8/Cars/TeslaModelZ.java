09549d8ef38298e7b55cfda9f2ce7be2a44461e4
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2020-03-24 18:45:27 -0400

    Polymorphism with cars.

---
+++ src/Unit8/Cars/TeslaModelZ.java
@@ -0,0 +1,31 @@
+package Unit8.Cars;
+
+public class TeslaModelZ extends ElectricCar implements SelfDriving {
+
+	int modelNum;
+
+	/** modelNum specifies the model number. Tesla cares about that
+	 stuff. Tesla Model Z’s have a 340 mile range on a full charge.*/
+	public TeslaModelZ(double startingMileage, int modelNum) {
+		super("Tesla", "Model Z", startingMileage, 340);
+		this.modelNum = modelNum;
+	}
+
+	/** Defaults mileage to 0. */
+	public TeslaModelZ(int modelNum) {
+		this(0, modelNum);
+	}
+
+	/** Returns the model number.*/
+	public int getModelNum() {
+		return this.modelNum;
+	}
+
+	@Override
+	public void driveAutonomously(double miles) {
+		if (miles > getRemainingRange()) {
+			super.drive(getRemainingRange());
+		}
+		super.drive(miles);
+	}
+}
