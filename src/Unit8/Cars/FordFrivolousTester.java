c5438cbfbe427a0d1c7e2638db5ccf05375ba89f
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2020-03-26 18:29:15 -0400

    Polymorphism with cars.

---
+++ src/Unit8/Cars/FordFrivolousTester.java
@@ -0,0 +1,8 @@
+package Unit8.Cars;
+
+public class FordFrivolousTester {
+	// By extension and implementation, also tests Car, GasPoweredCsr, Flying, & SelfDriving
+	public static void main(String[] args) {
+		// call different functions that test the aforementioned's behaviours down
+	}
+}
