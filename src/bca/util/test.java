5be44dd43d737bff6ccf2ddbbfe3e587a2bfb250
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2020-04-13 20:35:41 -0400

    Completed map lab.

---
+++ src/bca/util/test.java
@@ -0,0 +1,9 @@
+package bca.util;
+
+public class test {
+	public static void main(String[] args) {
+		Object o = new BCAArrayList();
+		Object p = new BCAArrayList();
+		System.out.println((o.equals(p)));
+	}
+}
