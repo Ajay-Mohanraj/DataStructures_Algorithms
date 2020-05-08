0bacbbac9c82f525d8bc5754bc47d2a1c52aa14e
Ajay-Mohanraj <ajaykmohanraj@gmail.com>
2020-05-08 14:11:51 -0400

    Completed hashed map linked list lab.

--- src/bca/util/test.java
+++ src/bca/util/test.java
@@ -2,20 +2,12 @@ package bca.util;
 
 import Unit8.Vehicle.FordFrivolous;
 
+import java.util.ArrayList;
+
 public class test {
 
-	static String newSeq;
-	public static String removeSeg(String oldSeq, String segment) {
-
-		int index = oldSeq.indexOf(segment);
-		String part2 = oldSeq.substring(index+segment.length());
-		String part1 = oldSeq.substring(0, index);
-		newSeq = part1+part2;
-		return (part1+part2);
-	}
 	public static void main(String[] args) {
-
-		String hello = "hello";
-		System.out.println(removeSeg(hello, "ll"));
+		ArrayList<Integer> b = new ArrayList<>();
+		System.out.println(1595000/100);
 	}
 }
